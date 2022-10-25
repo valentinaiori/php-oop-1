6c7cc54f8daa0e67c51531f20bedfbc1f0778fcf
valentinaiori <valentinaiori@live.it>
2022-10-25 17:07:32 +0200

    Add improvements

--- index.php
+++ index.php
@@ -26,21 +26,17 @@ class Movie{
 
 
 $movie1 = new Movie("Il discorso del re", "Tom Hooper", "storico-drammatico", "2010", "7.5");
-echo($movie1->title);
-echo($movie1->director);
-echo($movie1->genre);
-
 $movie2 = new Movie("Perfetti sconosciuti", "Paolo Genovese", "commedia", "2016", "8.5");
-echo($movie2->title);
-echo($movie2->director);
-echo($movie2->genre);
-
 
 $movie1->setVote(7.5);
 $movie2->setVote(8.5);
 
-
+echo "<pre>";
 var_dump($movie1);
+echo "</pre>";
+
+echo "<pre>";
 var_dump($movie2);
+echo "</pre>";
 
 ?>
